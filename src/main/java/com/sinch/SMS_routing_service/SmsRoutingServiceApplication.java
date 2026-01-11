48d043da336feac5f94feb68defbb6ed47db45b7
Rason Sze <rason.sze@ctint.com>
2026-01-11 11:01:49 +1100

    Initial commit with additional files

---
+++ src/main/java/com/sinch/SMS_routing_service/SmsRoutingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sinch.SMS_routing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsRoutingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsRoutingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sinch/SMS_routing_service/SmsRoutingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinch.SMS_routing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsRoutingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
